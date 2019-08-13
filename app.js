3d55b87d001fb0ab1ba027a4acb51f329674cee5
Angel-TheDev <angelantoinetted@yahoo.com>
2019-08-13 10:13:49 -0400

    Temporarily switched phone and email icon out in contact me

---
+++ app.js
@@ -0,0 +1,9 @@
+function pNumber(){
+    var pop = document.getElementById('showNumber');
+    pop.classList.toggle('show');
+}
+
+function email(){
+    var popup = document.getElementById('showEmail');
+    popup.classList.toggle('show');
+}
