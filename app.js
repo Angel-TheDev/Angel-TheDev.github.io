ca425247ac775ea9777ce6f5b52948459613ce92
Angel Davis <angeldavis.dev@Angels-MBP.attlocal.net>
2019-08-28 22:33:41 -0400

    iPad Responsive

--- app.js
+++
@@ -1,9 +0,0 @@
-function pNumber(){
-    var pop = document.getElementById('showNumber');
-    pop.classList.toggle('show');
-}
-
-function email(){
-    var popup = document.getElementById('showEmail');
-    popup.classList.toggle('show');
-}
---
+++ js/app.js
@@ -0,0 +1,13 @@
+// let coll = document.getElementsByClassName("project");
+// for (let i = 0; i < coll.length; i++) {
+//     coll[i].addEventListener("click", function () {
+//         this.classList.toggle("active");
+//         let resHolders = this.nextElementSibling;
+//         if (resHolders.style.maxHeight) {
+//             resHolders.style.maxHeight = null;
+//         } 
+//         else {
+//             resHolders.style.maxHeight = resHolders.scrollHeight + "px";
+//         }
+//     });
+// }
