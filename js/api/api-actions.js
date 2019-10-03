725767281a123c3583b7e6b6df473fc5486b897a
Angel Davis <angeldavis.dev@Angels-MBP.attlocal.net>
2019-10-02 21:48:35 -0400

    Node api - WIPI

--- js/api/api-actions.js
+++ js/api/api-actions.js
@@ -1,16 +1,16 @@
-function postRequest(location, requestBody, callback) {
-    fetch(location, {
-        method: 'POST',
-        body: JSON.stringify(requestBody),
-        headers: {
-            'Content-Type': 'application/json'
-        },
-    })
-    .then(res => res.json())
-    .then(jsonData => callback(jsonData))
-    .catch(err => console.log(err))
-    }
+// function postRequest(location, requestBody, callback) {
+//     fetch(location, {
+//         method: 'POST',
+//         body: JSON.stringify(requestBody),
+//         headers: {
+//             'Content-Type': 'application/json'
+//         },
+//     })
+//     .then(res => res.json())
+//     .then(jsonData => callback(jsonData))
+//     .catch(err => console.log(err))
+//     }
 
-    export default {
-        postRequest
-    };
+//     export default {
+//         postRequest
+//     };
--- js/app.js
+++ js/app.js
@@ -1,30 +1,30 @@
-import apiActions from './api/api-actions';
+// import apiActions from './api/api-actions';
 
-// Create event list for sub button
-let button = document.querySelector("#inputSubmit")
+// // Create event list for sub button
+// let button = document.querySelector("#inputSubmit")
 
-button.addEventListener("click", function (e) {
-    e.preventDefault()
-    // Onclick grab all of the data from the input boxes
-    console.dir(document.querySelector("#inputName").value);
-    const name = document.querySelector("#inputName").value;
-    const email = document.querySelector("#inputEmail").value;
-    const phone = document.querySelector("#inputPhone").value;
-    const message = document.querySelector("#inputMessage").value;
-    // console.log(name, email, phone, message)
-    // Put that data inside of an object
-    const emailOptions = {
-        name,
-        email,
-        phone,
-        message
-    }
-    //make a post to the api passing that object
+// button.addEventListener("click", function (e) {
+//     e.preventDefault()
+//     // Onclick grab all of the data from the input boxes
+//     console.dir(document.querySelector("#inputName").value);
+//     const name = document.querySelector("#inputName").value;
+//     const email = document.querySelector("#inputEmail").value;
+//     const phone = document.querySelector("#inputPhone").value;
+//     const message = document.querySelector("#inputMessage").value;
+//     // console.log(name, email, phone, message)
+//     // Put that data inside of an object
+//     const emailOptions = {
+//         name,
+//         email,
+//         phone,
+//         message
+//     }
+//     //make a post to the api passing that object
 
-    api-Actions.postRequest('https://cryptic-caverns-70902.herokuapp.com/email',
-        { method: 'post', body: JSON.stringify(emailOptions) })
-        .then (x => console.log("succeeded"));
-})
+//     api-Actions.postRequest('https://cryptic-caverns-70902.herokuapp.com/email',
+//         { method: 'post', body: JSON.stringify(emailOptions) })
+//         .then (x => console.log("succeeded"));
+// })
 
 
 
